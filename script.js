cf1e11f80585ed6cf335eff92d844ef53f94c50a
Lugdentube <samhayes1020@gmail.com>
2022-12-24 15:39:07 -0800

    add color change upon mouseover event

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+// create grid
 for (let i = 0; i < 16; i++) {
-  row = document.createElement("div");
+  let row = document.createElement("div");
   row.className = "row";
   for (let j = 0; j < 16; j++) {
-    cell = document.createElement("div");
+   let cell = document.createElement("div");
     cell.className = "cell"
       row.appendChild(cell);
   }
   document.querySelector(".game-container").appendChild(row);
 }
+
+// change color upon hovering
+const cells = document.querySelectorAll(".cell");
+cells.forEach((cell) => {
+  cell.addEventListener('mouseover', () => {
+    cell.classList.add("hovered");
+  });
+});
