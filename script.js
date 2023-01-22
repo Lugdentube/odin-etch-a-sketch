263c9190bc2270a571ea7e63f9232865c44dc7e2
Lugdentube <samhayes1020@gmail.com>
2023-01-22 14:58:14 -0800

    add slider to change board size

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-// create grid
-for (let i = 0; i < 16; i++) {
-  let row = document.createElement("div");
-  row.className = "row";
-  for (let j = 0; j < 16; j++) {
-   let cell = document.createElement("div");
-    cell.className = "cell"
-      row.appendChild(cell);
-  }
-  document.querySelector(".game-container").appendChild(row);
-}
+// generate initial grid
+createGrid(16);
 
-// change color upon hovering
-const cells = document.querySelectorAll(".cell");
-cells.forEach((cell) => {
-  cell.addEventListener('mouseover', () => {
-    cell.classList.add("hovered");
-  });
+// trigger board resize function after slider value change
+let slider = document.querySelector(".slider");
+slider.addEventListener('input', (e) => {
+  createGrid(e.target.value);
 });
+
+function createGrid(size) {
+  // update boardSize display
+  let boardSize = document.querySelector(".board-size")
+  boardSize.textContent = `${size} x ${size}`;
+  // grab parent element and wipe the current grid
+  let container = document.querySelector(".game-container")
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  // generate and append new grid to parent element
+  for (let i = 0; i < size; i++) {
+    let row = document.createElement("div");
+    row.className = "row";
+    for (let j = 0; j < size; j++) {
+      let cell = document.createElement("div");
+      cell.className = "cell"
+      row.appendChild(cell);
+    }
+  container.appendChild(row);
+  }
+  // add hover effect 
+  let cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.addEventListener('mouseover', () => {
+      cell.classList.add("hovered");
+    });
+  });
+}
